6aedd641c542b761a3085d882ca86a58bd121681
xingyi.hu <xingyi.hu@shopee.com>
2022-12-10 19:09:58 +0800

    feat: layout

---
+++ src/app/containers/HomePage/index.tsx
@@ -0,0 +1,27 @@
+import { type FC } from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+import Navbar from "../../components/navbar";
+import TopSection from "./topSection";
+
+const PageContainer = styled.div`
+  ${tw`
+    flex
+    flex-col
+    w-full
+    h-full
+    items-center
+    overflow-x-hidden
+  `};
+`;
+
+const HomePage: FC = () => {
+  return (
+    <PageContainer>
+      <Navbar />
+      <TopSection />
+    </PageContainer>
+  );
+};
+
+export default HomePage;
