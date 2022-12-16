7a6623beadbd8fc53f0baa8c43962ec5084ab096
xingyi.hu <xingyi.hu@shopee.com>
2022-12-16 22:10:44 +0800

    feat: footer of page

--- src/app/components/logo/index.tsx
+++ src/app/components/logo/index.tsx
@@ -2,6 +2,12 @@ import { type FC } from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 import CarLogoImg from "../../../assets/images/car-logo.png";
+import CarLogoDarkImg from "../../../assets/images/car-logo-dark.png";
+
+interface ILogoProps {
+  color?: "white" | "dark";
+  bgColor?: "white" | "dark";
+}
 
 const LogoContainer = styled.div`
   ${tw`
@@ -10,13 +16,16 @@ const LogoContainer = styled.div`
   `};
 `;
 
-const LogoText = styled.div`
+const LogoText = styled.div<{ color: "white" | "dark" }>`
   ${tw`
     text-xl
     md:text-2xl
     font-bold
     text-black
+    m-1
   `};
+  ${({ color }: { color: "white" | "dark" }) =>
+    color === "white" ? tw`text-white` : tw`text-black`}
 `;
 
 const Image = styled.div`
@@ -31,13 +40,15 @@ const Image = styled.div`
   }
 `;
 
-const Logo: FC = () => {
+const Logo: FC<ILogoProps> = (props: ILogoProps) => {
+  const { color, bgColor } = props;
+
   return (
     <LogoContainer>
       <Image>
-        <img src={CarLogoImg} alt="" />
+        <img src={bgColor === "dark" ? CarLogoDarkImg : CarLogoImg} alt="" />
       </Image>
-      <LogoText>Yourcar.</LogoText>
+      <LogoText color={color || "dark"}>Yourcar.</LogoText>
     </LogoContainer>
   );
 };
--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -1,7 +1,13 @@
 import { type FC } from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
+import BookCard from "../../components/bookCard";
+import Footer from "../../components/footer";
+import Marginer from "../../components/marginer";
 import Navbar from "../../components/navbar";
+import AboutUs from "./aboutUs";
+import BookingSteps from "./bookingSteps";
+import TopCars from "./topCars";
 import TopSection from "./topSection";
 
 const PageContainer = styled.div`
@@ -20,6 +26,15 @@ const HomePage: FC = () => {
     <PageContainer>
       <Navbar />
       <TopSection />
+      <Marginer direction="vertical" margin="4em" />
+      <BookCard />
+      <Marginer direction="vertical" margin="10em" />
+      <BookingSteps />
+      <Marginer direction="vertical" margin="8em" />
+      <AboutUs />
+      <Marginer direction="vertical" margin="8em" />
+      <TopCars />
+      <Footer />
     </PageContainer>
   );
 };
